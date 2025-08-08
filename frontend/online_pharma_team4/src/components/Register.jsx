13f5c6423eb4a510978a05e93ff25fd52b95e2ef
manilaxmisisvas <ssisvas@gmail.com>
2025-08-08 19:57:26 +0530

    Update Register.jsx
    changed backend url

--- frontend/online_pharma_team4/src/components/Register.jsx
+++ frontend/online_pharma_team4/src/components/Register.jsx
@@ -52,7 +52,7 @@ const Register = () => {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("https://online-pharma-backend.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userData),
