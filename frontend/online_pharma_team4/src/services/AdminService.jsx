aa7821a14b3bc616844302a1c4bf169a915ba8cb
manilaxmisisvas <ssisvas@gmail.com>
2025-08-08 20:01:09 +0530

    Update AdminService.jsx
    updated backend url

--- frontend/online_pharma_team4/src/services/AdminService.jsx
+++ frontend/online_pharma_team4/src/services/AdminService.jsx
@@ -1,7 +1,7 @@
 // src/services/AdminService.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api/admin";
+const API_BASE_URL = "https://online-pharma-backend.onrender.com/api/admin";
 
 const getAuthHeaders = () => ({
   Authorization: `Bearer ${localStorage.getItem("token")}`,
