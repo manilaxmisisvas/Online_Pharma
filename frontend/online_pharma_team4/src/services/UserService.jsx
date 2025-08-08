b8a86096bd8f52be1d9ce5bf98295244e8782730
manilaxmisisvas <ssisvas@gmail.com>
2025-08-08 20:03:27 +0530

    Update UserService.jsx
    updated backend url

--- frontend/online_pharma_team4/src/services/UserService.jsx
+++ frontend/online_pharma_team4/src/services/UserService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://online-pharma-backend.onrender.com/api/user";
 
 export const getUserProfile = async () => {
   const token = localStorage.getItem("token");
