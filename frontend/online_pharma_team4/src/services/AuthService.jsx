25b523566ffb715f47013d5231c6fa0772e7f245
manilaxmisisvas <ssisvas@gmail.com>
2025-08-08 20:01:54 +0530

    Update AuthService.jsx
    updated backend url

--- frontend/online_pharma_team4/src/services/AuthService.jsx
+++ frontend/online_pharma_team4/src/services/AuthService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = "https://online-pharma-backend.onrender.com/api/auth";
 
 export const login = async (credentials) => {
   const res = await axios.post(`${API_URL}/login`, credentials);
